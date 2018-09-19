4c76ada76b2d8dbcfa7d6040efa4773c1b1f37af
L-WJ <louwangjun_rf@foxmail.com>
2018-09-19 22:38:47 +0800

    2018-09-19 first submit

---
+++ http-server.js
@@ -0,0 +1,56 @@
+const port = 8081
+const url = require("url")
+const path = require("path")
+const http = require("http")
+const fs = require('fs')
+const mime = require('mime');
+const server_path = "C:/Users/Administrator/Desktop/qqq"
+
+function notfound(res) {
+    res.writeHead(404,{
+        'content-type':'text/plain;charset=utf-8'
+    })
+    res.write('404,页面不存在')
+    res.end()
+}
+
+let server = http.createServer((req,res) => {
+    let  pathname = url.parse(decodeURIComponent(req.url)).pathname
+    let  realPath = path.join(server_path, pathname)
+
+    fs.stat(realPath, (err,stats) => {
+        if (err) notfound(res) 
+        else {
+            if (stats.isDirectory()) {
+                fs.readdir(realPath,{withFileTypes: true}, (err,files) => {
+                    if (err) notfound(res) 
+                    else {
+                        res.writeHead(404,{
+                            'content-type':'text/html;charset=utf-8'
+                        })
+                        res.write(files.map(item => {
+                            let name = item.isDirectory() ? '/' : ''
+                            return `<div><a href = ./${item.name}${name}>${item.name}${name}</a></div>`
+                        }).join(""))
+                        res.end()
+                    }
+                })
+            } else {
+                fs.readFile(realPath, function(err,data){
+                    if(err) notfound(res)
+                    else{
+                      res.writeHead(200,{
+                        'content-type':mime.getType(data) + ';charset= utf-8'
+                      })
+                      res.write(data)
+                      res.end()
+                    }
+                  })
+            }
+        }
+    })
+
+    
+}).listen(port,() => {
+    console.log("Listenning successful")
+})
